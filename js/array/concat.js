d1488fffa311bd5712e8232c3af066fc3439ab5a
matheusbrantdev <matheuscarvalhocaldeirabrant@gmail.com>
2025-08-20 17:32:09 -0300

    feat: concat js array

---
+++ js/array/concat.js
@@ -0,0 +1,19 @@
+const array1 = ["a", "a","b", "c"];
+const array2 = ["d", "e", "f"];
+const array3 = array1.concat(array2);
+
+console.log(array3)
+// output: [  'a', 'a', 'b','c', 'd', 'e','f']
+
+
+const string1 = 'hello'
+const string2= 'wolrd'
+const hellowolrd = string1.concat(string2)
+
+console.log(hellowolrd)
+// output: hellowolrd
+
+const array = ["a", "b", "c"];
+
+const iterator = array.entries();
+
