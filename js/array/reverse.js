199a17a5b6890ef780064e27fc049ed36be10766
matheusbrantdev <matheuscarvalhocaldeirabrant@gmail.com>
2025-08-27 17:22:23 -0300

    feat: reverse

---
+++ js/array/reverse.js
@@ -0,0 +1,7 @@
+const array = ["one", "two", "three"];
+console.log("array:", array);
+// Expected output: "array:" Array ["one", "two", "three"]
+
+const reversed = array.reverse();
+console.log("reversed:", reversed);
+// Expected output: "reversed:" Array ["three", "two", "one"]
